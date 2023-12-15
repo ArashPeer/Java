e762c699ef0067ca92955762a9c472b6c82e6ac1
arashpeermohammad <arashperzai@yahoo.com>
2023-12-14 22:30:22 -0600

    ClassAndObject

---
+++ src/main/java_Dummy/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+
+    String name;
+    String model;
+    int doors;
+    boolean isPrime;
+    int makeYear;
+
+    public Car(){
+        name = "Toyota";
+        model = "4 doors corolla";
+        doors = 4;
+        isPrime = true;
+        makeYear = 2024;
+    }
+}
---
+++ src/main/java_Dummy/CarConstructor.java
@@ -0,0 +1,10 @@
+public class CarConstructor {
+    public static void main(String[] args) {
+        Car newCar = new Car();
+        System.out.println(newCar.doors);
+        System.out.println(newCar.isPrime);
+        System.out.println(newCar.name);
+        System.out.println(newCar.model);
+        System.out.println(newCar.makeYear);
+    }
+}
